a677ea94a47778dfb60bfad9200ce3878c3d082a
Yih-Chuan Lin <88579795+yclin88@users.noreply.github.com>
2025-10-27 13:49:59 +0800

    Update cbuiltindlg.cpp
    將視窗元件納入網格佈局管理

--- cbuiltindlg.cpp
+++ cbuiltindlg.cpp
@@ -14,6 +14,20 @@ CBuiltinDlg::CBuiltinDlg(QWidget *parent)
     progressPushBtn = new QPushButton(QStringLiteral("進度對話盒"));
     printPushBtn    = new QPushButton(QStringLiteral("列印對話盒"));
 
+    gridLayout->addWidget(colorPushBtn,0,0,1,1);
+    gridLayout->addWidget(errorPushBtn,0,1,1,1);
+    gridLayout->addWidget(filePushBtn,0,2,1,1);
+    gridLayout->addWidget(fontPushBtn,1,0,1,1);
+    gridLayout->addWidget(inputPushBtn,1,1,1,1);
+    gridLayout->addWidget(pagePushBtn,1,2,1,1);
+    gridLayout->addWidget(progressPushBtn,2,0,1,1);
+    gridLayout->addWidget(printPushBtn,2,1,1,1);
+    gridLayout->addWidget(displayTextEdit,3,0,3,3);
+    //以上是將QPushButton元件及QTextEdit納入gridLayout管理
+    setLayout(gridLayout); //將目前對話盒視窗設為gridLayout佈局
+    setWindowTitle(tr("內建對話盒展示"));
+    resize(400,300);
+
 }
 
 CBuiltinDlg::~CBuiltinDlg() {}
